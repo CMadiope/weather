2a3a69382a577e7471e13fbf39873a40bdc252e2
Confidence Madiope <confidencem97@gmail.com>
2022-07-20 16:20:47 +0200

    declared variables

---
+++ main.js
@@ -0,0 +1,4 @@
+let city = document.querySelector('.cityname')
+let btn = document.querySelector('.btn')
+let locBtn = document.querySelector('.locBtn')
+let output = document.querySelector('.output')
